44ccecb4ca424a5cefce92b7174fa98384b933ca
Aloento <11802769+Aloento@users.noreply.github.com>
2023-06-16 02:38:30 +0200

    DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,31 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\PostCate;
+use App\Models\Category;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->count(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory()->count(5)->create();
+
+        Post::factory()
+            ->count(10)
+            ->has(PostCate::factory()->count(2), 'categories')
+            ->has(Comment::factory()->count(3), 'comment')
+            ->create();
     }
 }
