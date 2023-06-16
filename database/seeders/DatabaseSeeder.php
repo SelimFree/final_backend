fb9c0705c62e87a1fbd963ad907f9375f8081334
nemavi <firiisi@gmail.com>
2023-06-16 09:18:38 +0200

    Backend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,31 +2,21 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use App\Models\User;
-use App\Models\Post;
-use App\Models\PostCate;
-use App\Models\Category;
-use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        User::factory()->count(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        Category::factory()->count(5)->create();
-
-        Post::factory()
-            ->count(10)
-            ->has(PostCate::factory()->count(2), 'categories')
-            ->has(Comment::factory()->count(3), 'comment')
-            ->create();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Article;
+use App\Http\Controllers\ArticleController;
+use App\Http\Resources\ArticleResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/article/{id}', function ($id) {
+    return new ArticleResource(Article::findOrfail($id));
+});
+
+Route::get('/articles', function () {
+    return  ArticleResource::collection(Article::all());
+});
+
+
+Route::put('/article/{id}',[ArticleController::class, 'update']);
+
+Route::delete('/article/{id}',[ArticleController::class, 'destroy']);
+
+Route::post('/articles',[ArticleController::class, 'store']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
